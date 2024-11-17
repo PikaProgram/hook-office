5ebea94430b9f1761434aa793f7e47624b9209a5
LiroSphere <61467599+PikaProgram@users.noreply.github.com>
2024-11-17 09:25:58 +0700

    feat(*): Grappling hook sticks to wall

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,39 @@
 extends CharacterBody2D
 
+@onready var left_border: CollisionShape2D = $"../World Borders/Left"
+@onready var right_border: CollisionShape2D = $"../World Borders/Right"
+@onready var player_collision_box: CollisionShape2D = $CollisionShape2D
+@onready var player: CharacterBody2D = $"."
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 700.0
+const HOOK_SPEED = -100
+const RELEASE_DISTANCE = 10
 
+var player_is_hooked = false
+var target_position = Vector2() # The Position of the hook head
+var click_position = Vector2()
+var hook_position = Vector2()
+
+
+func _ready() -> void:
+	velocity.y = -500
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if player_is_hooked:
+		if is_on_wall():
+			print("Player Unhooked")
+			player_is_hooked = false
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if Input.is_action_just_pressed("left_click"):
+			print("Player Hooked")
+			click_position = get_global_mouse_position()
+			player_is_hooked = true
+			velocity = (click_position - position).normalized() * SPEED
+
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
 	move_and_slide()
